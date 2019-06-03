fb9a41dea1f6d6866307cb67e8544cd2fb03005f
Ryan Gilfillan <regilfillan@gmail.com>
2019-06-03 02:00:00 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,17 +1,7 @@
 require 'bundler'
-# require "lib/concerns/finable.rb"
 Bundler.require
 
 module Concerns
-  module Findable
-    def find_by_name(name)
-    self.all.detect{|s| s.name == name}
-  end
-
-  def find_or_create_by_name(name)
-    self.find_by_name(name) || self.create(name)
-  end
-  end
 end
 
 require_all 'lib'
--- lib/song.rb
+++ lib/song.rb
@@ -39,13 +39,13 @@ class Song
     genre.songs << self unless genre.songs.include?(self)
   end
 
-  def self.find_by_name(name)
-    @@all.detect {|song| song.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@all.detect {|song| song.name == name}
+  # end
   
-  def self.find_or_create_by_name(name)
-    find_by_name(name) || create(name)
-  end
+  # def self.find_or_create_by_name(name)
+  #   find_by_name(name) || create(name)
+  # end
   
   def self.new_from_filename(filename)
     parts = filename.split(" - ")
